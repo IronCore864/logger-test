8187b7448ac195a52c89e2f9bb8d5fb24c71269b
Tiexin Guo <guotiexin@gmail.com>
2025-09-15 15:13:20 +0800

    refactor

--- logger/logger.go
+++ logger/logger.go
@@ -12,7 +12,7 @@ const (
 )
 
 type Logger interface {
-	Notice(msg string)
+	Noticef(format string, v ...any)
 }
 
 var (
@@ -23,7 +23,7 @@ var (
 func Noticef(format string, v ...any) {
 	loggerLock.Lock()
 	defer loggerLock.Unlock()
-	logger.Notice(fmt.Sprintf(format, v...))
+	logger.Noticef(format, v...)
 }
 
 func SetLogger(l Logger) {
@@ -32,26 +32,40 @@ func SetLogger(l Logger) {
 	loggerLock.Unlock()
 }
 
+// byteSliceWriter wraps a byte slice to implement io.Writer
+type byteSliceWriter struct {
+	buf *[]byte
+}
+
+func (w *byteSliceWriter) Write(p []byte) (n int, err error) {
+	*w.buf = append(*w.buf, p...)
+	return len(p), nil
+}
+
 type defaultLogger struct {
 	w      io.Writer
 	prefix string
-
-	buf []byte
+	buf    []byte
 }
 
-func (l *defaultLogger) Notice(msg string) {
+func (l *defaultLogger) Noticef(format string, v ...any) {
 	l.buf = l.buf[:0]
 	now := time.Now().UTC()
 	l.buf = now.AppendFormat(l.buf, timestampFormat)
 	l.buf = append(l.buf, ' ')
 	l.buf = append(l.buf, l.prefix...)
-	l.buf = append(l.buf, msg...)
-	if len(msg) == 0 || msg[len(msg)-1] != '\n' {
+	writer := &byteSliceWriter{buf: &l.buf}
+	fmt.Fprintf(writer, format, v...)
+	if len(l.buf) == 0 || l.buf[len(l.buf)-1] != '\n' {
 		l.buf = append(l.buf, '\n')
 	}
 	l.w.Write(l.buf)
 }
 
 func New(w io.Writer, prefix string) Logger {
-	return &defaultLogger{w: w, prefix: prefix}
+	return &defaultLogger{
+		w:      w,
+		prefix: prefix,
+		buf:    make([]byte, 0, 256), // Preallocate some reasonable capacity.
+	}
 }
