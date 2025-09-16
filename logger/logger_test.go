70e1afd83dd150343a682f40b295488b5e81e765
Tiexin Guo <guotiexin@gmail.com>
2025-09-16 14:22:44 +0800

    bench against os.stderr

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"io"
+	"os"
 	"testing"
 )
 
@@ -10,8 +11,7 @@ func BenchmarkLoggerNoticef(b *testing.B) {
 	logger := New(io.Discard, "Benchmark: ")
 	SetLogger(logger)
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		Noticef("Formatted message with number: %d and string: %s", 42, "test")
 	}
 }
@@ -19,6 +19,29 @@ func BenchmarkLoggerNoticef(b *testing.B) {
 // BenchmarkLoggerNoticefConcurrent tests concurrent logging performance.
 func BenchmarkLoggerNoticefConcurrent(b *testing.B) {
 	logger := New(io.Discard, "Benchmark: ")
+	SetLogger(logger)
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			Noticef("Concurrent message from goroutiner: %d and string: %s", 42, "test")
+		}
+	})
+}
+
+// BenchmarkLoggerNoticefStderr tests Noticef with real os.Stderr output.
+func BenchmarkLoggerNoticefStderr(b *testing.B) {
+	logger := New(os.Stderr, "Benchmark: ")
+	SetLogger(logger)
+
+	for b.Loop() {
+		Noticef("Formatted message with number: %d and string: %s", 42, "test")
+	}
+}
+
+// BenchmarkLoggerNoticefStderrConcurrent tests concurrent logging to os.Stderr.
+func BenchmarkLoggerNoticefStderrConcurrent(b *testing.B) {
+	logger := New(os.Stderr, "Benchmark: ")
 	SetLogger(logger)
 
 	b.ResetTimer()
